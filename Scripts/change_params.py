82f3eeb43589d85e11b072c7819b892d3d967b05
dwelling <>
2016-03-19 22:04:42 +0000

    Major update:       Finds start time by comparing event_list to IMF file       Uses existing param files in SWMF directory.       Option to turn plotting on (defaults to off).

--- Scripts/change_params.py
+++ Scripts/change_params.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
 '''
-
 This script copies the SWPCTEST param files from the Inputs directory, changes
 the start and stop times based on 'event_list.txt', and moves them into the 
 run directory corresponding to the event number.
+
+The start time is adjusted such that it is five hours earlier than the event
+start time.  If the imf argument is used, either the start time of the imf
+file OR the event start minus five hours is used, which ever yields the
+shortest simulation.
 
 EXAMPLE:
   > change_params.py 3 /nobackupp8/username/run_event3
@@ -13,6 +17,11 @@ EXAMPLE:
 
 ####### Defined functions:
 def parse_events(filename):
+    '''
+    Parse a file of event information.  Return as a dictionary, where each
+    entry is a dictionary containing 'start' time of event, 'end' time of
+    event (both datetime objects) and 'f107', the f10.7 solar radio flux.
+    '''
     import datetime as dt
     
     # Open file object:
@@ -41,8 +50,110 @@ def parse_events(filename):
     # Return data to caller:
     return events
 
+def get_imf_time(imf_file):
+    '''
+    For an imf file at location "imf_file", get the start time of the file
+    and return it to caller as a datetime object.
+    '''
+
+    from datetime import datetime
+
+    # Open file:
+    imf = open(imf_file, 'r')
+
+    # Search for start:
+    while '#START' not in imf.readline(): pass
+
+    # Extract start time and reduce spacing between numbers:
+    line = imf.readline()
+    time = ' '.join(line.split()[:6])
+
+    # Convert to datetime and return to user:
+    return datetime.strptime(time, '%Y %m %d %H %M %S')
+    
+def edit_param(old_file, new_file, event_info, plot=False):
+    '''
+    Read a PARAM.in file, "old_file", and replace start and end times, f10.7
+    flux, and other options to match a given event.  Write results to 
+    "new_file". 
+    '''
+    import re
+    
+    # Open old and new file:
+    old = open(old_file, 'r')
+    new = open(new_file, 'w')
+
+    # Loop through old file, fill in new file with changes:
+    while True:
+        # Read a line.  Break at EOF.
+        line = old.readline()
+        if not line: break
+
+        # Change start or end time:
+        if '#STARTTIME' in line or '#ENDTIME' in line:
+            if 'START' in line:
+                t = event_info['start']
+            else:
+                t = event_info['end']
+            new.write(line)
+            new.write(re.sub('\d{4}', '{:04d}'.format(t.year),   old.readline()))
+            new.write(re.sub('\d{2}', '{:02d}'.format(t.month),  old.readline()))
+            new.write(re.sub('\d{2}', '{:02d}'.format(t.day),    old.readline()))
+            new.write(re.sub('\d{2}', '{:02d}'.format(t.hour),   old.readline()))
+            new.write(re.sub('\d{2}', '{:02d}'.format(t.minute), old.readline()))
+            new.write(re.sub('\d{2}', '{:02d}'.format(t.second), old.readline()))
+
+        # Change F10.7 setting:
+        elif '#IONOSPHERE' in line:
+            f107 = '{:.1f}'.format(event_info['f107'])
+            new.write(line)
+            new.write(old.readline()) # TypeConductance
+            new.write(old.readline()) # UseFullCurrent
+            new.write(old.readline()) # UseRegion2
+            new.write(re.sub('\d+\.\d+', f107, old.readline()))
+            new.write(old.readline()) # Starlight
+            new.write(old.readline()) # PolarCapPed
+
+        # Turn off plots by removing '#':
+        elif not plot and '#SAVEPLOT' in line and '#SAVEPLOTNAME' not in line:
+            new.write(line[1:])
+
+        # Change magnetometer grid files:
+        elif '#MAGNETOMETERGRID' in line:
+            # If no plotting, turn them off.
+            if not plot:
+                new.write(line[1:])
+                continue
+            # Else, turn up resolution.
+            new.write(line)           # #MAGNETOMETER GRID
+            new.write(old.readline()) # ascii
+            new.write(old.readline()) # GEO
+            new.write('72'+old.readline()[1:]) # higher lon res.
+            new.write('33'+old.readline()[1:]) # higher lat res.
+
+        # Disable satellite includes:
+        elif '! Satellite' in line:
+            old.readline()
+            old.readline()
+
+        # Reduce restart saving frequency:
+        elif '#SAVERESTART' in line:
+            new.write(line)
+            new.write(old.readline())
+            new.write(old.readline())
+            new.write('360'+old.readline()[1:])
+            
+        # Echo all other lines:
+        else:
+            new.write(line)
+            
+    # Close both files.
+    old.close()
+    new.close()
+    
 ####### Script:
 import os
+from datetime import timedelta
 from argparse import ArgumentParser
 
 # Default values:
@@ -52,7 +163,11 @@ basepath   = os.path.realpath(__file__).replace('change_params.py', '../')
 parser = ArgumentParser(description=__doc__)
 parser.add_argument('event', type=int, help='The event number.')
 parser.add_argument('rundir', help='Path of the run directory.')
-                    
+parser.add_argument('-p', '--plot', action='store_true',
+                    help='Keep SAVEPLOT commands.')
+parser.add_argument('-i','--imf', default='', help='IMF file name corresponding'+
+                    ' to PARAM file.  If provided, start time in PARAM will not'+
+                    ' preceed first time listed in IMF file.')
 parser.add_argument('-f', '--event_file', default=basepath+'/event_list.txt',
                     help="Set location of 'event_file.txt' which contains the "+
                     'details of each event.  Default is to search in SWPCTEST' +
@@ -68,76 +183,18 @@ if not os.path.exists(args.rundir):
 # Load event info:
 event = parse_events(args.event_file)[args.event]
 
-## INIT FILE ##
-old = open(basepath+'/Inputs/PARAM.in_SWPC_init', 'r')
-new = open(args.rundir+'/PARAM.in_SWPC_init', 'w')
-
-# Read file, replacing start and end times as well as F10.7:
-while True:
-    line = old.readline()
-    if '#STARTTIME' in line:
-        t = event['start']
-        new.write(line)
-        new.write(old.readline().replace('XXXX', '{:04d}'.format(t.year)))
-        new.write(old.readline().replace('XXXX', '{:02d}'.format(t.month)))
-        new.write(old.readline().replace('XXXX', '{:02d}'.format(t.day)))
-        
-        new.write(old.readline().replace('XXXX', '{:02d}'.format(t.hour)))
-        new.write(old.readline().replace('XXXX', '{:02d}'.format(t.minute)))
-        new.write(old.readline().replace('XXXX', '{:02d}'.format(t.second)))
-    elif '#ENDTIME' in line:
-        t = event['end']
-        new.write(line)
-        new.write(old.readline().replace('XXXX', '{:04d}'.format(t.year)))
-        new.write(old.readline().replace('XXXX', '{:02d}'.format(t.month)))
-        new.write(old.readline().replace('XXXX', '{:02d}'.format(t.day)))
-        
-        new.write(old.readline().replace('XXXX', '{:02d}'.format(t.hour)))
-        new.write(old.readline().replace('XXXX', '{:02d}'.format(t.minute)))
-        new.write(old.readline().replace('XXXX', '{:02d}'.format(t.second)))
-        new.write('0.0		FracSecond\n\n')
-        new.write('#END '+40*'#'+'\n')
-        break
-    elif '#IONOSPHERE' in line:
-        new.write(line)
-        new.write(old.readline()) # TypeConductance
-        new.write(old.readline()) # UseFullCurrent
-        new.write(old.readline()) # UseRegion2
-        new.write(old.readline().replace('XXXX','{:.1f}'.format(event['f107'])))
-        new.write(old.readline()) # Starlight
-        new.write(old.readline()) # PolarCapPed
-    else:
-        new.write(line)
-new.close()
-
-## RESTART FILE ##
-old = open(basepath+'/Inputs/PARAM.in_SWPC_restart', 'r')
-new = open(args.rundir+'/PARAM.in_SWPC_restart', 'w')
-
-# Read file, replacing start and end times as well as F10.7:
-while True:
-    line = old.readline()
-    if '#ENDTIME' in line:
-        t = event['end']
-        new.write(line)
-        new.write(old.readline().replace('XXXX', '{:04d}'.format(t.year)))
-        new.write(old.readline().replace('XXXX', '{:02d}'.format(t.month)))
-        new.write(old.readline().replace('XXXX', '{:02d}'.format(t.day)))
-        
-        new.write(old.readline().replace('XXXX', '{:02d}'.format(t.hour)))
-        new.write(old.readline().replace('XXXX', '{:02d}'.format(t.minute)))
-        new.write(old.readline().replace('XXXX', '{:02d}'.format(t.second)))
-        new.write('0.0		FracSecond\n\n')
-        new.write('#END '+40*'#'+'\n')
-        break
-    elif '#IONOSPHERE' in line:
-        new.write(line)
-        new.write(old.readline()) # TypeConductance
-        new.write(old.readline()) # UseFullCurrent
-        new.write(old.readline()) # UseRegion2
-        new.write(old.readline().replace('XXXX','{:.1f}'.format(event['f107'])))
-        new.write(old.readline()) # Starlight
-        new.write(old.readline()) # PolarCapPed
-    else:
-        new.write(line)
-new.close()
+# Get start time from IMF file.
+if args.imf:
+    t_imf=get_imf_time(args.imf)
+else:
+    t_imf=event['start']-timedelta(hours=5)
+    
+# Turn back start time by 5 hours.  Ensure start >= time_imf_min
+event['start'] = max( event['start']-timedelta(hours=5), t_imf)
+    
+# Change init file:
+edit_param(basepath+'../Param/SWPC/PARAM.in_SWPC_init',
+           args.rundir+'/PARAM.in_SWPC_init',    event, args.plot)
+# Change restart file:
+edit_param(basepath+'../Param/SWPC/PARAM.in_SWPC_restart',
+           args.rundir+'/PARAM.in_SWPC_restart', event, args.plot)
