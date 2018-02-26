76d917e37742c68f9e5e01c82c67449f74ef5156
dwelling <>
2018-02-26 22:28:17 +0000

    Simplified replace regex for F107 so that it works consistently with the Young et al. class of commands.

--- Scripts/change_param.pl
+++ Scripts/change_param.pl
@@ -94,7 +94,7 @@ while(<>){
 	$_ .= $end;
     }
     # Set F10.7 flux in the line containinf "F107Flux"
-    s/.*\s(F107Flux)/$f107\t\t\t$1/i;
+    s/.*\s(F107)/$f107\t\t\t$1/i;
 
     # Switch off saving large plots and restart files
     s/^\#((?:SAVEPLOT)|(?:MAGNETOMETERGRID)|(?:SAVERESTART))\b/$1/ 
