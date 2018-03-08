c53643b12c3f04f79809aec15c2fd1dfd783e560
igorsok <>
2018-03-08 06:54:35 +0000

    1. Add magnetic coordinates of station to the magnetometer file 2. Improve the list of stations involved. 3. Report a list of stations to the file MAGNETOMETER.in including    magnetic coordinates.

--- Scripts/magnetometer.py
+++ Scripts/magnetometer.py
@@ -5,36 +5,42 @@ import math
 '''
 IAGA codes of Russian and Ukrainian stations
 '''
-AllRus='AIA ARS BOX KLI CPS IRT KHB KIV LVV MGD MOS NVS PET YAK'#KZN
-RusRealTime ='KLI MOS'#ARS    #Stations which provide data in real time
+AllRus='AIA ARS BOX KLI CPS IRT KHB KIV KZN LVV MGD MOS NVS PET SPG YAK'
+RusRealTime ='ARS KLI MOS SPG' #Stations which provide data in real time
 '''
 Same for American stations incuding Alaska, Guam, Honolulu, Puerto Rico
 ''' 
 AmeRealTime='BOU BRW BSL CMO DED FRD FRN GUA HON NEW SHU SIT SJG TUC'
-AllAme=AmeRealTime#+' HAD+KAK HER'
+AllAme=AmeRealTime      #+' HAD HER KAK'
+ArcticSTA = 'amd bbg dik loz pbk tik viz'
+"""
+Erroneous or missing data, if present on one the first three
+components:
+"""
 Error  = '99999'
+ErrMessage = 'Error='+Error+' in line ' 
 Stations = []
 IgrfName  = os.path.realpath(__file__).replace(
     'magnetometer.py', '../../bin/IGRF.exe')
+
 Min2Rad = math.pi/10800  #\pi/180/60
 import subprocess
 def magin():
     """
     Make magnetometer file with all station names and coordinates
     """ 
-    os.system('rm -f magin_GEM.dat')
-    iFileAll = open('magin_GEM.dat','w')
+    os.system('rm -f MAGNETOMETER.in')
+    iFileAll = open('MAGNETOMETER.in','w')
     iFileAll.write('#COORD\n')
-    iFileAll.write('GEO\n\n')
+    iFileAll.write('MAG\n\n')
     iFileAll.write(
         'List of '+str(len(Stations))+' stations plus DST\n')
     iFileAll.write('#START\n')
     iFileAll.write('DST 360.0 360.0\n')
     for MetaData in Stations:
-        iFileAll.write(
-            MetaData[0]+' '+MetaData[1]+' '+MetaData[2]+'\n')
+        iFileAll.write(' '.join(MetaData)+'\n')
     iFileAll.close()
-def http_download(Station,URL,DateTimeStart):
+def http_download(Station,URL,DateTimeStart,IsNoBase):
     import urllib2
     print 'Download '+URL
     WriteMagFileName = Station+'.txt'
@@ -49,13 +55,13 @@ def http_download(Station,URL,DateTimeStart):
     iFileAux.write(str(
             DateTimeStart.year+DateTimeStart.timetuple().tm_yday/365.0)
                    +'\n')
+    nData = 0
     for line in Lines:
         if '|' in line:   # Header lines
             if 'Latitude' in line:
                 LineSplit=line.split()
                 Latitude = LineSplit[2]
                 iFileAux.write(Latitude+'\n')
-                MetaData.append(Latitude)
                 WriteMagFile.write(line)
             elif 'Longitude' in line:
                 LineSplit=line.split()
@@ -64,8 +70,6 @@ def http_download(Station,URL,DateTimeStart):
                     Longitude =str(
                         float(Longitude) + 360.0)
                 iFileAux.write(Longitude+'\n') 
-                MetaData.append(Longitude)
-                Stations.append(MetaData)
                 WriteMagFile.write(line)
             elif 'Elevation' in line:
                 LineSplit=line.split()
@@ -130,15 +134,22 @@ def http_download(Station,URL,DateTimeStart):
                         TrimLine = IgrfLine[l+3:len(IgrfLine)]
                         TrimLineSplit = TrimLine.split()
                         Z0 = int(TrimLineSplit[0])
+                    elif 'Latitude' in IgrfLine:
+                        l = IgrfLine.index('Latitude')
+                        TrimLine = IgrfLine[l+8:len(IgrfLine)]
+                        TrimLineSplit = TrimLine.split()
+                        MetaData.append(TrimLineSplit[0])
+                    elif 'Longitude' in IgrfLine:
+                        l = IgrfLine.index('Longitude')
+                        TrimLine = IgrfLine[l+9:len(IgrfLine)]
+                        TrimLineSplit = TrimLine.split()
+                        MetaData.append(TrimLineSplit[0])
                     WriteMagFile.write(IgrfLine)
                 IgrfFile.close()
                 LineSplit = line.split()
-                LineWrite = LineSplit[0]+'     '+LineSplit[1]
-                LineWrite = LineWrite+'     '+Station+'H'
-                LineWrite = LineWrite+'     '+Station+'E'
-                LineWrite = LineWrite+'     '+Station+'Z'
-                LineWrite=LineWrite+'                             '
-                LineWrite = LineWrite+'|\n'
+                LineWrite = '     '.join(
+                    LineSplit[0:2]+[Station+'H',Station+'E',
+                    Station+'Z'])+29*' '+'|\n'
                 WriteMagFile.write(LineWrite)
             elif 'http' in line:
                 WriteMagFile.write(line)
@@ -148,80 +159,84 @@ def http_download(Station,URL,DateTimeStart):
                 WriteMagFile.write(line)
         else:          #End of header
             LineSplit = line.split()
-            if Error in LineSplit[3]+' '+LineSplit[4]+' '+LineSplit[5]:
-                pass
+            if Error in ' '.join(LineSplit[3:6]):
+                if nData>0:
+                    print ErrMessage+"  ".join(LineSplit[0:6])
             else:      #Good data line
-                TimeSplit = LineSplit[1].split('.')
-                LineWrite = LineSplit[0]+' '+TimeSplit[0]+' '
+                nData = nData +1
+                LineWrite = LineSplit[0]+' '+LineSplit[1]+' '
                 if IsHDZ:
-                    """
-                    Transform HD=>XY
-                    """
                     H = float(LineSplit[3])
                     D = float(LineSplit[4])*Min2Rad
-                    X = H*math.cos(D)
-                    Y = H*math.sin(D)
+                    if IsNoBase:
+                        """
+                        Convert variation D to E 
+                        """
+                        E = H0*math.tan(D)
+                        Z = float(LineSplit[5])
+                        IsXYZ = False
+                    else:
+                        """
+                        Transform HD=>XY
+                        """
+                        X = H*math.cos(D)
+                        Y = H*math.sin(D)
                 elif IsXYZ:
                     X = float(LineSplit[3])
                     Y = float(LineSplit[4]) 
+                else:
+                    """
+                    HEZ 
+                    """
+                    H = float(LineSplit[3])
+                    E = float(LineSplit[4])
+                    Z = float(LineSplit[5])
                 if IsXYZ:
                     """
                     Transform XY=>HE using IGRF declination
                     """
                     H = X*CosDecl0 + Y*SinDecl0
                     E = Y*CosDecl0 - X*SinDecl0
-                    Z = float(LineSplit[5])
-                    if 'AIA' in Station:
-                        """
-                        XYZ variations, no base field included
-                        """
-                        pass
-                    else:
-                        """
-                        Subtract IGRF H,Z field
-                        """
-                        H = H - H0
-                        Z = Z - Z0
-                    LineWrite = LineWrite+'{0:-8.2f}'.format(H)
-                    LineWrite = LineWrite+' '+'{0:-8.2f}'.format(E)+' '
-                    LineWrite = LineWrite+'{0:-8.2f}'.format(Z)+'\n'   
-                else:
+                    Z = float(LineSplit[5])  
+                if not IsNoBase:
                     """
-                    HEZ components, subtract IGRF H,Z field
+                    Subtract IGRF H,Z field
                     """
-                    H = float(LineSplit[3]) - H0
-                    Z = float(LineSplit[5]) - Z0
-                    LineWrite = LineWrite+'{0:-8.2f}'.format(H)
-                    LineWrite = LineWrite+' '+LineSplit[4]+' '
-                    LineWrite = LineWrite+'{0:-8.2f}'.format(Z)+'\n' 
-            WriteMagFile.write(LineWrite) #Good data line is written
+                    H = H - H0
+                    Z = Z - Z0
+                LineWrite = LineWrite+'{0:-8.2f}'.format(H)
+                LineWrite = LineWrite+' '+'{0:-8.2f}'.format(E)+' '
+                LineWrite = LineWrite+'{0:-8.2f}'.format(Z)+'\n'  
+                WriteMagFile.write(LineWrite) #Good data line is written
     """
     No more lines
     """
     WriteMagFile.close()
+    if nData > 1:
+        """
+        If data are not missing
+        """
+        Stations.append(MetaData)
     os.system('tail -1 '+WriteMagFileName)
-def download(NameRus,NameAme,DateTimeStart,DateTimeEnd):
+def download(NameRus,NameAme,NoBase,DateTimeStart,DateTimeEnd):
+    """
+    In some station the base field is not added to the data and 
+    there is no need to subtract it, to obtain variation 
+    Call with NoBase='' to avoid subtracting base field, if desired  
+    """
     '''
     -start download magnetometer files remove older one
     '''
     os.system("rm -f *.txt")
-    TimeEnd       = time(
-        DateTimeEnd.hour,DateTimeEnd.minute,DateTimeEnd.second)
-    DateEnd       = date(
-        DateTimeEnd.year,DateTimeEnd.month,DateTimeEnd.day)
-    EndString     = DateEnd.isoformat()+'T'+TimeEnd.isoformat()
-    TimeStart     = time(
-        DateTimeStart.hour,DateTimeStart.minute,DateTimeStart.second)
-    DateStart     = date(
-        DateTimeStart.year,DateTimeStart.month,DateTimeStart.day)
-    StartString   = DateStart.isoformat()+'T'+TimeStart.isoformat()
+    EndString     = DateTimeEnd.strftime("%Y-%m-%dT%H:%M:%S")
+    StartString   = DateTimeStart.strftime("%Y-%m-%dT%H:%M:%S")
     Prefix        = 'http://geomag.gcras.ru/intermagnet-webapp/'
     Prefix        = Prefix+'IntermagnetAscii?station='
     Suffix =    '&format=iaga2002&table=pre_min&timeFrom='
     Delimiter = '&timeTo='
     for Station in NameRus.split():
         URL = Prefix+Station+Suffix+StartString+Delimiter+EndString
-        http_download(Station,URL,DateTimeStart)
+        http_download(Station,URL,DateTimeStart,Station in NoBase)
         
     '''
     America stations 
@@ -233,14 +248,14 @@ def download(NameRus,NameAme,DateTimeStart,DateTimeEnd):
         URL = 'https://geomag.usgs.gov/ws/edge/?id='+Station
         URL = URL+'&starttime='+StartString+'Z&endtime='
         URL = URL+EndString+'Z&elements=H,E,Z'
-        http_download(Station,URL,DateTimeStart)
+        http_download(Station,URL,DateTimeStart,False)
 def realtime(nDays):
     DateTimeEnd   = datetime.utcnow()
     DateEnd       = date(
         DateTimeEnd.year,DateTimeEnd.month,DateTimeEnd.day)    
     DateTimeStart = datetime.combine(
         DateEnd,time(0,0,0)) - timedelta(days=nDays-1)
-    download(RusRealTime,AmeRealTime,DateTimeStart,DateTimeEnd)
+    download(RusRealTime,AmeRealTime,'ARS', DateTimeStart,DateTimeEnd)
     os.system('rm -f *.aux')
     magin()
 if __name__ == '__main__':
@@ -257,21 +272,19 @@ if __name__ == '__main__':
     DateStart = args.DateStart
     nDays     = args.nDays
     if '-' in DateStart:
-        DateStartSplit = DateStart.split('-')
-        DateTimeStart = datetime.combine(
-            date(int(DateStartSplit[0]),
-                 int(DateStartSplit[1]),
-                 int(DateStartSplit[2])),time(0,0,0))
+        DateTimeStart = datetime.strptime(DateStart,'%Y-%m-%d')
         DateTimeEnd = DateTimeStart + timedelta(days=nDays,minutes=-1)
-        download(AllRus,AllAme,DateTimeStart,DateTimeEnd)
+        download(AllRus,AllAme,'AIA KZN',DateTimeStart,DateTimeEnd)
         """
         Arctic stations
         """
         import ftplib
         ftp = ftplib.FTP('imagftp.gcras.ru','polarguest','ytpfuheprf')
-        ArcticSTA = 'amd bbg dik loz pbk tik viz'
         for sta in ArcticSTA.split():
-            DirName = '/'+sta+'/'+DateStartSplit[0]
+            if 'pbk' in sta and DateTimeStart>datetime.strptime(
+                '2017-07-31','%Y-%m-%d'):
+                sta = 'brn' # No data for Pevel after this date
+            DirName = '/'+sta+'/'+str(DateTimeStart.year)
             ftp.cwd(DirName)
             WriteMagFileName = sta.upper()+'.txt'
             WriteMagFile = open(WriteMagFileName,'w')
@@ -281,16 +294,11 @@ if __name__ == '__main__':
                     DateTimeStart.year
                     + DateTimeStart.timetuple().tm_yday/365.0)
                            +'\n')
+            nData = 0
             for x in range(0, nDays):
                 local = open('local.aux','wb')
                 DateTime = DateTimeStart + timedelta(days=x) 
-                DateTimeString = DateTime.isoformat()
-                DateTimeStringSplit = DateTimeString.split('T')
-                DateString = DateTimeStringSplit[0]
-                DateStringSplit = DateString.split('-')
-                DateString = DateStringSplit[0]+DateStringSplit[1]
-                DateString = DateString+DateStringSplit[2]
-                filename= sta+DateString+'vmin.min'
+                filename= sta+DateTime.strftime("%Y%m%d")+'vmin.min'
                 print 'Download '+DirName+'/'+filename
                 ftp.retrbinary("RETR "+filename,local.write)
                 local.close()
@@ -305,7 +313,6 @@ if __name__ == '__main__':
                                 LineSplit=line.split()
                                 Latitude = LineSplit[2]
                                 iFileAux.write(Latitude+'\n')
-                                MetaData.append(Latitude)
                             if 'Longitude' in line:
                                 LineSplit=line.split()
                                 Longitude = LineSplit[2]
@@ -313,40 +320,60 @@ if __name__ == '__main__':
                                     Longitude =str(
                                         float(Longitude) + 360.0)
                                 iFileAux.write(Longitude+'\n') 
-                                MetaData.append(Longitude)
-                                Stations.append(MetaData)
                             if 'Elevation' in line:
                                 LineSplit=line.split()
                                 iFileAux.write(
                                     str(float(LineSplit[1])*0.001)+'\n')
                                 iFileAux.close()
                             if 'DATE' in line:
+                                iFileAux.close()
+                                iFileAux = open('data.aux','r')
+                                IgrfFile = open('igrf.aux','w')
+                                subprocess.call(
+                                    IgrfName,stdin=iFileAux,stdout=IgrfFile)
+                                iFileAux.close()
+                                IgrfFile.close()
+                                IgrfFile = open('igrf.aux','r')
+                                IgrfLines = IgrfFile.readlines()
+                                for IgrfLine in IgrfLines:
+                                    if 'Latitude' in IgrfLine:
+                                        l = IgrfLine.index('Latitude')
+                                        TrimLine = IgrfLine[l+8:len(IgrfLine)]
+                                        TrimLineSplit = TrimLine.split()
+                                        MetaData.append(TrimLineSplit[0])
+                                        WriteMagFile.write(IgrfLine)
+                                    elif 'Longitude' in IgrfLine:
+                                        l = IgrfLine.index('Longitude')
+                                        TrimLine = IgrfLine[l+9:len(IgrfLine)]
+                                        TrimLineSplit = TrimLine.split()
+                                        MetaData.append(TrimLineSplit[0])
+                                        WriteMagFile.write(IgrfLine)
+                                IgrfFile.close()
                                 '''
                                 Remove day of year
                                 '''
                                 LineSplit = line.split()
-                                LineWrite = LineSplit[0]+'   '+LineSplit[1]
-                                LineWrite = LineWrite+'   '+LineSplit[3]
-                                LineWrite = LineWrite+'   '+LineSplit[4]
-                                LineWrite = LineWrite+'   '+LineSplit[5]
-                                LineWrite = LineWrite+'                   '
-                                LineWrite = LineWrite+'                  '
-                                LineWrite = LineWrite+'|\n'
-                               
+                                LineWrite = '   '.join(
+                                    LineSplit[0:2]+LineSplit[3:6])+37*' '+'|\n'
                                 WriteMagFile.write(LineWrite)
                             else:
                                 WriteMagFile.write(line)
                     else:
                         LineSplit = line.split()
-                        TimeSplit = LineSplit[1].split('.')
-                        LineWrite = LineSplit[0]+' '+TimeSplit[0]
-                        LineWrite = LineWrite+' '+LineSplit[3]
-                        LineWrite = LineWrite+' '+LineSplit[4]
-                        LineWrite = LineWrite+' '+LineSplit[5]+'\n'
+                        LineWrite = ' '.join(
+                            LineSplit[0:2])+'  '+"  ".join(
+                            LineSplit[3:6])+'\n'
                         if Error in LineWrite:
-                            pass
+                            if nData>0:
+                                print ErrMessage+" ".join(LineSplit[0:6])
                         else:
+                            nData = nData +1
                             WriteMagFile.write(LineWrite)
+            if nData > 1:
+                """
+                If data are not missing
+                """
+                Stations.append(MetaData)
             WriteMagFile.close()
             os.system('tail -1 '+WriteMagFileName)
         os.system('rm -f *.aux')
