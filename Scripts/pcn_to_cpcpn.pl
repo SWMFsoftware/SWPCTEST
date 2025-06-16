214fd860aefec8b7c2f587adf8835f2fa344e6bb
Gabor Toth <gtoth@umich.edu>
2025-06-16 13:43:53 -0400

    Fixed PCN formulat, updated CPCPN.txt for Event20, added script for south.

--- Scripts/pcn_to_cpcpn.pl
+++ Scripts/pcn_to_cpcpn.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl -p
 # Usage: pcn_to_cpcpn.pl pcn.log > CPCPN.txt
-s/pcn/pcn cpcp/;
+s/pcn/pcn cpcpn/;
 next unless /^[\d\.\+\-\s]+$/;
 @a = split;
-$cpcp = 29.28 - sin(2*3.1415926*($a[1]-1)/12+1.49) + 17.81*$a[-1];
+$pcn = $a[7]; $pcn = 0 if $pcn < 0;
+$cpcp = 29.28 - 3.31*sin(2*3.1415926*($a[1]-1)/12+1.49) + 17.81*$pcn;
 s/\n/ $cpcp\n/;
